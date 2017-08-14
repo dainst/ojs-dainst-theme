6bf8481a36e618c48e99ceeb3153c405de13aa19
Philipp Franck <philipp.franck@dainst.de>
2017-08-14 11:15:48 +0200

    italian activated and merged changes

--- DainstThemePlugin.inc.php
+++ DainstThemePlugin.inc.php
@@ -405,7 +405,7 @@ class DainstThemePlugin extends ThemePlugin {
 		// the colorsheme color
 		$dainstcicolor = ($journal) ? $this->getSetting($journal->getId(), 'dainstcicolor') : 'components'; 
 		$templateMgr->assign('dainstcicolor', $dainstcicolor);
-		$templateMgr->assign('languageToggleLocalesHacked', array('de_DE' => 'Deutsch', 'en_US' => 'English', 'es_ES' => 'Español'));
+		$templateMgr->assign('languageToggleLocalesHacked', array('de_DE' => 'Deutsch', 'en_US' => 'English', 'es_ES' => 'Español', 'it_IT' => 'Italiano'));
 
 		// register functions for idai-components-php
 		$smarty->register_block("idai_navbar", array($this, "getNavbar"));
